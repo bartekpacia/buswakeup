095a937a4c3277318a48413fa5c05065850a78d5
Bartek Pacia <barpac02@gmail.com>
2019-03-27 13:58:02 +0100

    CurrentLocationListener is now lifecycle-aware

--- app/src/main/java/pl/baftek/buswakeup/MainActivity.kt
+++ app/src/main/java/pl/baftek/buswakeup/MainActivity.kt
@@ -1,8 +1,10 @@
 package pl.baftek.buswakeup
 
 import android.content.Intent
+import android.location.Location
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,15 +15,19 @@ class MainActivity : AppCompatActivity() {
 
         buttonMap.setOnClickListener { startActivity(Intent(this, MapsActivity::class.java)) }
         buttonStartService.setOnClickListener {
-            val input = editText.text.toString()
-
             val intent = Intent(this, LocationService::class.java)
-            intent.putExtra("input", input)
+            intent.putExtra("input", editText.text.toString())
             startService(intent)
         }
         buttonStopService.setOnClickListener {
             val intent = Intent(this, LocationService::class.java)
             stopService(intent)
         }
+
+        CurrentLocationListener.getInstance(applicationContext).observe(this, Observer<Location> { location: Location? ->
+            location?.let {
+                textLocation.text = "Lat: ${location.latitude} Long: ${location.longitude}"
+            }
+        })
     }
 }
