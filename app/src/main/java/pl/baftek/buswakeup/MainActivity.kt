8429fe5556eec2b641a50e74fd13e8cd3d1c9c61
Bartek Pacia <barpac02@gmail.com>
2019-03-27 16:24:56 +0100

    Deleted unnecesary things

--- app/src/main/java/pl/baftek/buswakeup/MainActivity.kt
+++ app/src/main/java/pl/baftek/buswakeup/MainActivity.kt
@@ -1,10 +1,8 @@
 package pl.baftek.buswakeup
 
 import android.content.Intent
-import android.location.Location
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,22 +12,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val serviceIntent = Intent(this, LocationService::class.java)
 
         buttonMap.setOnClickListener { startActivity(Intent(this, MapsActivity::class.java)) }
-        buttonStartService.setOnClickListener {
-            val intent = Intent(this, LocationService::class.java)
-            intent.putExtra("input", editText.text.toString())
-            startService(intent)
-        }
-        buttonStopService.setOnClickListener {
-            val intent = Intent(this, LocationService::class.java)
-            stopService(intent)
-        }
 
-        CurrentLocationListener.getInstance(applicationContext).observe(this, Observer<Location> { location: Location? ->
-            location?.let {
-                textLocation.text = "Lat: ${location.latitude} Long: ${location.longitude}"
+        serviceSwitch.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                startService(serviceIntent)
+            } else {
+                stopService(serviceIntent)
             }
-        })
+        }
     }
 }
