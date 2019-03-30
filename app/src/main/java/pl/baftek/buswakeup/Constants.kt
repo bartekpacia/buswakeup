1b4870431d0b9436c2c9a2c831962d879d2b1c39
Bartek Pacia <barpac02@gmail.com>
2019-03-30 15:55:27 +0100

    -Room DB improvements (location still doesn't work)

--- app/src/main/java/pl/baftek/buswakeup/Constants.kt
+++ app/src/main/java/pl/baftek/buswakeup/Constants.kt
@@ -1,4 +1,7 @@
 package pl.baftek.buswakeup
 
+import com.google.android.gms.maps.model.LatLng
+
 const val CHANNEL_ID = "locationServiceChannel"
 const val DATABASE_NAME = "buswakeup-database"
+val start = LatLng(50.098915, 18.551711)
