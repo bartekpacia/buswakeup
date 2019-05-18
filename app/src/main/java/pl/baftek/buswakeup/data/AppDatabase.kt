7fed1bdea838f45a2841bc10dc328ff0f82fffd0
Bartek Pacia <barpac02@gmail.com>
2019-05-18 23:39:17 +0200

    added fallbackToDestructiveMigration()

--- app/src/main/java/pl/baftek/buswakeup/data/AppDatabase.kt
+++ app/src/main/java/pl/baftek/buswakeup/data/AppDatabase.kt
@@ -32,6 +32,7 @@ abstract class AppDatabase : RoomDatabase() {
         private fun buildDatabase(context: Context): AppDatabase {
             return Room.databaseBuilder(context, AppDatabase::class.java, DATABASE_NAME)
                     .allowMainThreadQueries()
+                    .fallbackToDestructiveMigration()
                     .build()
         }
     }
